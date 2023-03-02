6b37068c9d414a2f4ccb0a2c5da59d60318a19a1
soukaina203 <soukaina9mourabit@gmail.com>
2023-03-02 05:11:24 +0000

    using ressources routes

--- routes/api.php
+++ routes/api.php
@@ -17,46 +17,15 @@ Route::post('/signUp',[userController::class,'signup']);
 Route::post('/login',[userController::class,'login']);
 // CRUD  produits
 Route::group(['middleware'=> ['auth:sanctum']], function(){
-    Route::controller(ProductController::class)->group(function () {
-        Route::get('/products', 'index');
-        Route::get('/product/show/{id}', 'show');
-        Route::get('/Products/edit/{id}', 'edit');
-        Route::post('/Product/add', 'store');
 
-        Route::patch('/Products/{id}', 'update');
-        Route::delete('/product/delete/{id}', 'destroy');
+    Route::resources([
+        'products' => ProductController::class,
+        'suppliers' => supplierController::class,
+        'clients' => ClientController::class,
+        'companies' => ClientController::class,
+        'users' => ClientController::class,
+    ]);
 
-    }    );
-// CRUD  clients
 
-    Route::controller(ClientController::class)->group(function () {
-        Route::get('/clients', 'index');
-        Route::get('/client/{id}', 'show');
-        Route::get('/client/edit/{id}', 'edit');
 
-        Route::post('/client/add', 'store');
-        Route::patch('/client/{id}', 'update');
-
-        Route::delete('/client/d/{id}', 'destroy');
-    });
-// CRUD  fournisseur
-
-Route::controller(supplierController::class)->group(function () {
-    Route::get('/suppliers', 'index');
-    Route::get('/supplier/{id}', 'show');
-    Route::get('/supplier/edit/{id}', 'edit');
-
-    Route::post('/supplier/add', 'store');
-    Route::patch('/supplier/{id}', 'update');
-
-    Route::delete('/supplier/d/{id}', 'destroy');
 });
-});
-// $table->string('FullName');
-// $table->string('phoneNumber');
-// $table->string('email');
-// $table->string('Localisation');
-// $table->string('fonction');
-// $table->string('otherContact');
-// $table->string('rating');
-// protected $fillable=['FullName','phoneNumber','email','Localisation','fonction','otherContact','rating'];
